b451423a39715d31968c032ec442ebfec86e6f51
Santosh Boga <49557995+SantoshBoga@users.noreply.github.com>
2026-08-18 14:57:54 -0700

    Add: duplicate-integer - submission-2

---
+++ Data Structures & Algorithms/duplicate-integer/submission-2.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean hasDuplicate(int[] nums) {
+        Set<Integer> uniques = new HashSet<>();
+        for (Integer num : nums) {
+            if (uniques.contains(num)) {
+                return true;
+            }
+            else {
+                uniques.add(num);
+            }
+        }
+        return false;
+    }
+}
